8c04fd20788c6186211a615eab6c35804652519e
jrousseau <v4lproik@gmail.com>
2016-04-07 22:27:25 +0200

    [redis] Fix tests

--- src/main/java/net/v4lproik/spamshouldnotpass/platform/client/redis/RedisTestConfiguration.java
+++ src/main/java/net/v4lproik/spamshouldnotpass/platform/client/redis/RedisTestConfiguration.java
@@ -1,10 +1,26 @@
 package net.v4lproik.spamshouldnotpass.platform.client.redis;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
-@Configuration
 @Import(value = {
         ConfigRedis.class
 })
-public class RedisTestConfiguration {}
+public class RedisTestConfiguration {
+
+    private final JedisConnectionFactory connectionFactory;
+
+    @Autowired
+    public RedisTestConfiguration(JedisConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    JedisConnection connection(){
+        return connectionFactory.getConnection();
+    }
+
+}
--- src/test/java/net/v4lproik/spamshouldnotpass/platform/client/redis/ConfigRedisITest.java
+++ src/test/java/net/v4lproik/spamshouldnotpass/platform/client/redis/ConfigRedisITest.java
@@ -3,22 +3,24 @@ package net.v4lproik.spamshouldnotpass.platform.client.redis;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
         classes = {
                 RedisTestConfiguration.class,
         })
+@WebAppConfiguration
 public class ConfigRedisITest {
 
     @Autowired
-    ConfigRedis configRedis;
+    public JedisConnection connection;
 
     @Test
     public void test_ping_redis(){
-        configRedis.connectionFactory().getConnection().ping();
+        connection.ping();
     }
-
 }
